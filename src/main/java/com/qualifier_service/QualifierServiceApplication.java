5d0d8fcfbb4d3c6afaeb1c5a8a1905bba14188d5
Joel <joel.viana@audora.com.br>
2024-09-04 20:26:26 -0300

    first commit

---
+++ src/main/java/com/qualifier_service/QualifierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qualifier_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualifierServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qualifier_service/QualifierServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qualifier_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualifierServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
